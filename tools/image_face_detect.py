aa99275cd92a9b7c6f2b639f8810be78e4844297
Tosuke Iwanaga <tousuke.21@gmail.com>
2024-09-15 11:41:58 +0900

    Update image_face_detect.py

--- tools/image_face_detect.py
+++ tools/image_face_detect.py
@@ -1,23 +1,29 @@
 import numpy as np
 import cv2 as cv
 
+cap = cv.VideoCapture(0)
+
 face_cascade = cv.CascadeClassifier('tools/haarcascade_frontalface_default.xml')
 eye_cascade = cv.CascadeClassifier('tools/haarcascade_frontalface_default.xml')
 
-img = cv.imread('tools/image.jpeg')
+while True:
+    # img = cv.imread('tools/image.jpeg')
+    ret, frame = cap.read()
 
-gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
-faces = face_cascade.detectMultiScale(gray, 1.15, 8)
-print(len(faces))
+    gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, 1.15, 8)
+    print(len(faces))
 
-for (x, y, w, h) in faces:
-    cv.rectangle(img, (x,y), (x+w, y+h), (255,0,0), 10)
-    eye_gray = gray[y:y+h, x:x+w]
-    eye_color = img[y:y+h, x:x+w]
-    eyes = eye_cascade.detectMultiScale(eye_gray)
-    for (ex, ey, ew, eh) in eyes:
-        cv.rectangle(eye_color, (ex, ey), (ex+ew, ey+eh),(0,255,0), 10)
+    for (x, y, w, h) in faces:
+        cv.rectangle(frame, (x,y), (x+w, y+h), (255,0,0), 10)
+        eye_gray = gray[y:y+h, x:x+w]
+        eye_color = frame[y:y+h, x:x+w]
+        eyes = eye_cascade.detectMultiScale(eye_gray)
+        for (ex, ey, ew, eh) in eyes:
+            cv.rectangle(eye_color, (ex, ey), (ex+ew, ey+eh),(0,255,0), 10)
 
-cv.imshow('img',img)
-cv.waitKey(0)
+    cv.imshow('img',frame)
+    if cv.waitKey(1) & 0xFF == ord("q"):
+        break
+    
 cv.destroyAllWindows()
